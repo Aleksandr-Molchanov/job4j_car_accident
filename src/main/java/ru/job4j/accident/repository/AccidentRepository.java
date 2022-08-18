a07811a50bb7dba41de8c1276e74b77436a7b0de
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2022-08-18 09:43:56 +0300

    2. Spring Data [#296073 #332844]

--- src/main/java/ru/job4j/accident/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accident/repository/AccidentRepository.java
@@ -3,5 +3,16 @@ package ru.job4j.accident.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accident.model.Accident;
 
+import java.util.Optional;
+
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
+
+    @Override
+    <S extends Accident> S save(S s);
+
+    @Override
+    Optional<Accident> findById(Integer integer);
+
+    @Override
+    Iterable<Accident> findAll();
 }
